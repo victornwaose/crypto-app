ef14205480c0b19db0f0c23f63dfc0e4738afbe4
nwaose victor <nwaosevictor1234@gmail.com>
2021-11-12 15:59:17 +0100

    changes in login and sign-up

--- src/components/authentication/authentication/Login.js
+++ src/components/authentication/authentication/Login.js
@@ -1,4 +1,11 @@
-import { TextField, Box, Button } from "@material-ui/core";
+import {
+    TextField,
+    Box,
+    Button,
+    createTheme,
+    makeStyles,
+    ThemeProvider,
+} from "@material-ui/core";
 import React, { useState } from "react";
 import { signInWithEmailAndPassword } from "@firebase/auth";
 import { useHistory } from "react-router";
@@ -7,6 +14,12 @@ import { CryptoState } from "../../../context/CryptoContext";
 import { auth } from "../../../Firebase";
 
 const Login = ({ handleClose }) => {
+    const useStyle = makeStyles((theme) => ({
+        title: {
+            color: "white",
+        },
+    }));
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const history = useHistory();
@@ -43,48 +56,60 @@ const Login = ({ handleClose }) => {
             });
         }
     };
+    const classes = useStyle();
+
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main: "#fff",
+            },
+            type: "dark",
+        },
+    });
 
     return (
-        <Box
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                gap: "20px",
-                color: "#fff",
-            }}
-            p={3}
-        >
-            <TextField
-                variant="outlined"
-                type="email"
-                color="#fff"
-                focused
-                label="Enter Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                fullWidth
-                style={{ border: "1px solid  #fff", borderRadius: 5 }}
-            />
-            <TextField
-                variant="outlined"
-                type="password"
-                label="Enter A Password"
-                color="#fff"
-                focused
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                style={{ border: "1px solid  #fff", borderRadius: 5 }}
-                fullWidth
-            />
-            <Button
-                variant="contained"
-                size="large"
-                style={{ background: "#EEBC1D", color: "#fff" }}
-                onClick={handleSubmit}
+        <ThemeProvider theme={darkTheme}>
+            {" "}
+            <Box
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "20px",
+                    color: "#fff",
+                }}
+                p={3}
             >
-                Sign-In
-            </Button>
-        </Box>
+                <TextField
+                    variant="outlined"
+                    type="email"
+                    label="Enter Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    fullWidth
+                    style={{ border: "1px solid  #fff", borderRadius: 5 }}
+                    className={classes.title}
+                />
+                <TextField
+                    variant="outlined"
+                    type="password"
+                    label="Enter A Password"
+                    color="#Secondary"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    style={{ border: "1px solid  #fff", borderRadius: 5 }}
+                    className={classes.title}
+                    fullWidth
+                />
+                <Button
+                    variant="contained"
+                    size="large"
+                    style={{ background: "#EEBC1D", color: "#fff" }}
+                    onClick={handleSubmit}
+                >
+                    Sign-In
+                </Button>
+            </Box>
+        </ThemeProvider>
     );
 };
 
--- src/components/authentication/authentication/SignUp.js
+++ src/components/authentication/authentication/SignUp.js
@@ -1,12 +1,24 @@
 import { createUserWithEmailAndPassword } from "@firebase/auth";
-import { Box, Button, TextField } from "@material-ui/core";
 import React, { useState } from "react";
 import { useHistory } from "react-router";
+import {
+    TextField,
+    Box,
+    Button,
+    createTheme,
+    makeStyles,
+    ThemeProvider,
+} from "@material-ui/core";
 
 import { CryptoState } from "../../../context/CryptoContext";
 import { auth } from "../../../Firebase";
 
 const SignUp = ({ handleClose }) => {
+    const useStyle = makeStyles((theme) => ({
+        title: {
+            color: "white",
+        },
+    }));
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -39,66 +51,78 @@ const SignUp = ({ handleClose }) => {
         } catch (error) {}
     };
 
+    const classes = useStyle();
+
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main: "#fff",
+            },
+            type: "dark",
+        },
+    });
     return (
-        <Box
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                gap: "20px",
-                color: "#fff",
-            }}
-            p={3}
-        >
-            <TextField
-                variant="outlined"
-                type="email"
-                label="Enter A Name"
-                color="#fff"
-                focused
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                fullWidth
-                style={{ border: "1px solid  #fff", borderRadius: 5 }}
-            />
-            <TextField
-                variant="outlined"
-                color="#fff"
-                type="email"
-                focused
-                label="Enter Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                style={{ border: "1px solid  #fff", borderRadius: 5 }}
-                fullWidth
-            />
-            <TextField
-                variant="outlined"
-                type="password"
-                label="Enter A Password"
-                focused
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                style={{ border: "1px solid  #fff", borderRadius: 5 }}
-                fullWidth
-            />
-            <TextField
-                variant="outlined"
-                type="password"
-                label="Confirm your Password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                style={{ border: "1px solid  #fff", borderRadius: 5 }}
-                fullWidth
-            />
-            <Button
-                variant="contained"
-                size="large"
-                style={{ background: "#EEBC1D", color: "#fff" }}
-                onClick={handleSubmit}
+        <ThemeProvider theme={darkTheme}>
+            <Box
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "20px",
+                    color: "#fff",
+                }}
+                p={3}
             >
-                SIGN UP
-            </Button>
-        </Box>
+                <TextField
+                    variant="outlined"
+                    type="email"
+                    label="Enter A Name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    fullWidth
+                    style={{ border: "1px solid  #fff", borderRadius: 5 }}
+                />
+                <TextField
+                    variant="outlined"
+                    type="email"
+                    label="Enter Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    style={{ border: "1px solid  #fff", borderRadius: 5 }}
+                    fullWidth
+                />
+                <TextField
+                    variant="outlined"
+                    type="password"
+                    label="Enter A Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    style={{ border: "1px solid  #fff", borderRadius: 5 }}
+                    fullWidth
+                />
+                <TextField
+                    variant="outlined"
+                    type="password"
+                    color="success"
+                    label="Confirm your Password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    style={{
+                        border: "1px solid  #fff",
+                        borderRadius: 5,
+                        color: "#fff",
+                    }}
+                    fullWidth
+                />
+                <Button
+                    variant="contained"
+                    size="large"
+                    style={{ background: "#EEBC1D", color: "#fff" }}
+                    onClick={handleSubmit}
+                >
+                    SIGN UP
+                </Button>
+            </Box>
+        </ThemeProvider>
     );
 };
 
